328b6092bfd79428d6c2e102d02443ddea829bb7
Tim Su <tim@todoroo.com>
2012-04-25 21:33:17 -0700

    redis port and stuff

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+CONFIG = YAML.load_file(Rails.root.join("config/redis.yml"))[Rails.env]
+
+$redis = Redis.new(:host => CONFIG['host'] || "localhost", :port => CONFIG["port"] || 6379)
 
