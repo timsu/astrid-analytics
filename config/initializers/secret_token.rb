a7b6c07934b3a12af7b9bba57c3ae900b2fe49ad
Tim Su <tim@todoroo.com>
2012-04-25 17:33:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AstridAnalytics::Application.config.secret_token = 'ee66ecee4f5f6a7cfb59ae87397fa6e6d802346ab4aeda39395690e51f401f1fdbe18639d6f7de16da351a4259249ad6cccdc859b8d85721f1b9a833500762cf'
