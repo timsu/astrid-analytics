2d189b966a39e89cc77e21e0d1f24e3274a7df8f
Tim Su <tim@todoroo.com>
2012-04-25 19:26:49 -0700

    Account adding stuff

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -21,7 +21,7 @@ class AdminController < ApplicationController
 
   def index
     @accounts = $redis.smembers "accounts"
-    
+
     @data = @accounts.reduce({}) do |data, account|      
       data[account] = JSON.parse($redis.get "#{account}:data")
       data
@@ -31,6 +31,43 @@ class AdminController < ApplicationController
       data[account] = $redis.smembers "#{account}:apikeys"
       data
     end
+  end
+
+  def add_account
+    if request.post?
+      salt = rand(36**6).to_s(36)
+      account = {
+        :id => $redis.scard("accounts") + 1,
+        :name => params[:name],
+        :email => params[:email],
+        :salt => salt,
+        :password => Digest::MD5.hexdigest(params[:password] + salt)
+      }
+        
+      $redis.sadd "accounts", account[:id]
+      $redis.get "#{account[:id]}:data", account.to_json
+    end
+
+    redirect_to "/admin"
+  end
+
+  def add_client
+    if request.post?
+      account = params[:account]
+
+      render :text => "Error: unknown account" unless $redis.sismember "accounts", account
+
+      begin
+        apikey = rand(36**6).to_s(36)
+      end while $redis.get("apikeys:#{apikey}")
+      client = params[:client]
+      secret = rand(36**6).to_s(36)
+        
+      $redis.sadd "#{account}:apikeys", apikey
+      $redis.set "apikeys:#{apikey}", [account, client, secret]
+    end
+
+    redirect_to "/admin"
   end
   
   ################################################################# HELPERS
