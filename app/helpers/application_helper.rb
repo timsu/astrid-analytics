26a948ad71b0a607bd5b6a81db2a0a3f3e62642b
Tim Su <tim@astrid.com>
2012-05-25 10:34:49 -0400

    henrypolish - formatting of ios title, making the screen support media query

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,24 @@
 module ApplicationHelper
 
-  def number(num)
+  def format_number(num)
     number_to_human num, :units => {:unit => "", :thousand => "K", :million => "M"}
   end
 
-  def percent(pct)
+  def format_percent(pct)
     if pct.instance_of? Float
       number_to_percentage pct, :significant => true
     else
       pct
+    end
+  end
+
+  def format_client(client)
+    if client == "ios"
+      "iOS"
+    elsif client == "iphone"
+      "iPhone"
+    else
+      client.capitalize
     end
   end
 
