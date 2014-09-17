937654abd8ea8adfd0d2247ec5d413670f2f991a
Tim Su <timsu@yahoo-inc.com>
2014-09-16 18:55:14 -0700

    Remove airbrake gem

--- config/initializers/airbrake.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = 'e02d023139929e5d5561dffb7ca9683a'
-  config.host = 'errors.astrid.com'
-  config.port = 80
-  config.secure = config.port == 443
-end
